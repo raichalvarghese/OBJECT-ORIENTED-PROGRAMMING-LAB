11bc281a3309570bf24ff1d1725f45ae8a74309a
Raichal <raichalvarghese231@gmail.com>
2022-04-11 08:13:13 +0530

    07/04 co1-co2

--- CO1/Q5/cpu.java
+++ CO1/Q5/cpu.java
@@ -1,35 +1,54 @@
-public class CPU{
-	int price;
-	
-	class processor{
-		int cores;
-		String manufact;
-		processor(int cr,String manufacturer){
-			cores = cr;
-			manufact = manufacturer;
-		}
-		void display(){
-			System.out.println("Number of cores:"+cores);
-			System.out.println("Manufacturer:"+ manufact);
-		}
-	}
-	static class ram{
-		int mem;
-		String manu;
-		ram(int memory,String manufactr){
-			mem= memory;
-			manu = manufactr;
-		}
-		void display(){
-			System.out.println("Memory:"+mem);
-			System.out.println("Manufacturer:"+ manu);
-		}
-	}
-	public static void main(String[] args){
-		CPU.ram obj1 = new CPU.ram(8,"INTEL");
-		CPU obj2=new CPU();
-		obj1.display();
-		CPU.processor obj3 = obj2.new processor(1,"Samsung");
-		obj3.display();
-	}
+import java.util.Scanner;
+
+public class cpu {
+    double price;
+        public class processor
+        {
+            float cores;
+            String mfg;
+            void display(float n, String pname)
+            {
+                cores = n;
+                mfg = pname;
+                System.out.println("The processor has "+cores+" cores from the "+mfg+" manufacturer");
+            }
+        }
+            static class RAM
+            {
+                float mem;
+                String mfg;
+                void display(float n, String pname)
+                {
+                    mem = n;
+                    mfg = pname;
+                    System.out.println("RAM memory is "+mem+" and the manufacturer is "+mfg);
+                }
+            }
+                public static void main(String[] args) 
+                {
+                    cpu obj = new cpu();
+                    cpu.processor obj1 = obj.new processor();
+                    cpu.RAM obj2 = new cpu.RAM();
+                    Scanner sc = new Scanner(System.in);
+                    System.out.print("Enter the price of the CPU:");
+                    obj.price = sc.nextDouble();
+                    System.out.println("Processor Details:-");
+                    System.out.println("Enter the number of cores:");
+                    float c = sc.nextFloat();
+                    sc.nextLine();
+                    System.out.println("Enter the processor name:");
+                    String pname = sc.nextLine();
+                    
+                    System.out.println("Enter RAM details:-");
+                    System.out.println("Enter the size of the RAM:");
+                    float mem = sc.nextFloat();
+                    sc.nextLine();
+                    System.out.println("Enter the Manufacturer:");
+                    String mfg = sc.nextLine();
+                    
+                    System.out.println("Price of the CPU is "+obj.price);
+                    obj1.display(c, pname);
+                    obj2.display(mem, mfg);
+                    
+                }
 }
